9c965126688a4039746eaba2d0704f118f46da6d
shahinbayvari <shahinbayvari@gmail.com>
2022-05-26 09:38:18 -0400

    Finally add paddle yay

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,13 @@
-public class Board {
+import javax.swing.*;
+import java.awt.*;
+
+public class Board extends JPanel {
+    public Board() {
+
+    }
+    public void paint(Graphics g) {
+        // paddle
+        g.setColor(Color.BLACK);
+        g.fillRect(310, 550, 100, 8);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         Board board = new Board();
+        frame.setBounds(200, 100, 700, 600);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.add(board);
     }
 }
