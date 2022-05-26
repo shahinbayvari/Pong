f7e1817917ab4d93a726d2fefe5b2ead86b9c2fd
shahinbayvari <shahinbayvari@gmail.com>
2022-05-26 07:32:01 -0400

    Add board

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        JFrame board = new Board();
+        board.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        board.setLocationRelativeTo(null);
+        board.setResizable(false);
+        board.pack();
     }
 }
