7ec0b2e3b7f02d0ff548a3f138155c3cf11b4a57
shahinbayvari <shahinbayvari@gmail.com>
2022-05-26 09:39:18 -0400

    Set size screen & center screen

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         Board board = new Board();
-        frame.setBounds(200, 100, 700, 600);
+        frame.setSize(1000, 1000);
+        frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
