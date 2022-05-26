fff4686e23d4306cb32bbd580a11dcff16179b82
shahinbayvari <shahinbayvari@gmail.com>
2022-05-26 09:57:10 -0400

    Add paddles & ball

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 
 public class Board extends JPanel {
     public Board() {
 
     }
     public void paint(Graphics g) {
-        // paddle
+        // paddle 1
         g.setColor(Color.BLACK);
-        g.fillRect(310, 550, 100, 8);
+        g.fillRect(950, 450, 10, 100);
+
+        // paddle 2
+        g.setColor(Color.BLACK);
+        g.fillRect(50, 450, 10, 100);
+
+        // ball
+        g.setColor(Color.BLACK);
+        g.fillRect(490, 490, 20, 20);
     }
 }
